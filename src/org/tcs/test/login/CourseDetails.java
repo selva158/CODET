6c069f76a6fcfa4129ff7fe187befd5382ed7136
suresh158 <selvasundaram0102@gmail.com>
2025-02-02 18:17:30 +0530

    new code added

---
+++ src/org/tcs/test/login/CourseDetails.java
@@ -0,0 +1,28 @@
+package org.tcs.test.login;
+
+
+	public class CourseDetails {
+
+		
+
+		private void javaCourse() {
+			// TODO Auto-generated method stub
+			System.out.println("java course is taken"); 
+			
+			
+		}
+		private void pythonCourse() {
+			
+			System.out.println("python course is not taken");
+			
+		}
+		public static void main(String[] args) {
+			CourseDetails cd=new CourseDetails();
+			cd.javaCourse();
+			cd.pythonCourse();
+		}
+
+	}
+				
+
+
